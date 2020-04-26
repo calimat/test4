81eaa2292414ee986b022f0694f1574560c74bac
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-04-26 00:03:15 -0500

    Initial Commit

---
+++ test4/test4.h
@@ -0,0 +1,19 @@
+//
+//  test4.h
+//  test4
+//
+//  Created by Ricardo Herrera Petit on 4/26/20.
+//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test4.
+FOUNDATION_EXPORT double test4VersionNumber;
+
+//! Project version string for test4.
+FOUNDATION_EXPORT const unsigned char test4VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test4/PublicHeader.h>
+
+
